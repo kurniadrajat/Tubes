d55bfc0ff22ec3537b4314e819230db3326cb6a3
kurniadrajat <46840733+kurniadrajat@users.noreply.github.com>
2019-04-27 11:28:46 +0700

    Mengganti Define relation
    seperti infoR,nextR

--- relasi.h
+++ relasi.h
@@ -2,46 +2,44 @@
 #define RELASI_H_INCLUDED
 
 #include <iostream>
-#include "pasien.h"
-#include "dokter.h"
+#include "child.h"
+#include "parent.h"
 
-#define next(P) P->next
-#define prev(P) P->prev
-#define first(L) L.first
-#define last(L) L.last
+#define nextR(P) P->nextR
+#define prevR(P) P->prevR
+#define firstR(L) L.firstR
+#define lastR(L) L.lastR
 #define child(P) P->child
 #define parent(P) P->parent
+#define infoR(P) P->infoR
 
 using namespace std;
 
-typedef struct elmlist_relasi *address_relasi;
+typedef struct elmlistrelation *addressrelation;
 
-struct elmlist_relasi{
-    address_relasi next;
-    address_relasi prev;
-    address_child child;
-    address_parent parent;
+struct elmlistrelation{
+    addressrelation nextR;
+    addressrelation prevR;
+    addressparent parent;
+    addresschild child;
 };
 
-struct list_relasi{
-    address_relasi first;
-    address_relasi last;
+struct listrelation{
+    addressrelation firstR;
+    addressrelation lastR;
 };
 
-void createList(list_relasi &L);
-void insertFirst(list_relasi &L, address_relasi P);
-void insertLast(list_relasi &L, address_relasi P);
-void inserAfter(list_relasi &L, address_relasi Prec, address_relasi P);
-void deleteFirst(list_relasi &L, address_relasi &P);
-void deleteLast(list_relasi &L, address_relasi &P);
-void deleteAfter(list_relasi &L, address_relasi Prec, address_relasi &P);
-address_relasi alokasi(address_parent P, address_child C);
-void dealokasi_relasi(address_relasi &P);
-address_relasi findByID(list_relasi L, address_parent P, address_child C);
-void printInfo(list_relasi L);
-void deleteRelasi(list_relasi &LR, list_child LC, list_parent LP, infotype_child CC, infotype_parent PP, address_relasi &RR);
-bool cek_id(list_relasi LR, address_child C, address_parent P);
-void sorting(list_relasi &L);
+inline void createlist_relation(listrelation &L);
+inline void insertfirst_relation(listrelation &L,addressrelation P);
+inline void insertlast_relation(listrelation &L,addressrelation P);
+inline void insertafter_relation(listrelation &L,addressrelation P , addressrelation prec);
+inline void deletefirst_relation(listrelation &L,addressrelation &P);
+inline void deletelast_relation(listrelation &L,addressrelation &P);
+inline void deleteafter_relation(listrelation &L,addressrelation &P, addressrelation prec);
+inline void printinfo_relation(listrelation L);
+inline void delete_relation(listrelation &LR,listchild LC ,listparent LP ,infotypechild CC,infotypeparent PP,addressrelation &RR);
+addressrelation searchnim_relation(listrelation L,addressparent P,addresschild C);
+
 
 
 #endif // RELASI_H_INCLUDED
